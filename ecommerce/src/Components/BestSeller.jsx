c857d096ea68b577d058698a5f1d4a13f817388e
sandipshres <sandipshrestha4455@gmail.com>
2026-01-29 17:58:04 +0545

    completion of product page

--- ecommerce/src/Components/BestSeller.jsx
+++ ecommerce/src/Components/BestSeller.jsx
@@ -1,8 +1,27 @@
 import styles from './BestSeller.module.css'
 import Card from './Card.jsx'
+import {useState,useEffect} from 'react'
 import picture from '../assets/winter.png'
 
 function BestSeller(){
+    let data;
+    
+    const [products,setProducts] = useState([]);
+
+    useEffect(()=>{
+        async function getProducts() {
+            try{
+            let res = await fetch('/jsonFiles/bestProducts.json');
+            data = await res.json();
+            setProducts(data.products);
+            // console.log(data);
+            // console.log(data.products);
+            }catch(err){
+                console.log(err);
+            }
+        }
+        getProducts();
+    },[]);
     return(
         <div className={styles.bestSection} id="bestSeller">
         <div className = {styles.heading}>  
@@ -10,15 +29,14 @@ function BestSeller(){
             <p>The product everyone is talking about.</p>
         </div>
         <div className={styles.CardsContainer}>
-            <Card src={picture} acPrice={200} discount={10} title="This is nothing" />
-            <Card src={picture} acPrice={200} discount={10} title="This is nothing" />
-            <Card src={picture} acPrice={200} discount={10} title="This is nothing" />
-            <Card src={picture} acPrice={200} discount={10} title="This is nothing" />
-            <Card src={picture} acPrice={200} discount={10} title="This is nothing" />
-            <Card src={picture} acPrice={200} discount={10} title="This is nothing" />
-            <Card src={picture} acPrice={200} discount={10} title="This is nothing" />
-            <Card src={picture} acPrice={200} discount={10} title="This is nothing" />
-            <Card src={picture} acPrice={200} discount={10} title="This is nothing" />
+          {
+            products.map(product=>{
+                console.log(product.title);
+                return(
+                <Card key={product.productKey} src={product.MainImage} acPrice={product.price} discount={product.discount} title={product.title} />
+                )
+            })
+          }
         </div>
         </div>
     )
--- ecommerce/src/Components/ProductPage.jsx
+++ ecommerce/src/Components/ProductPage.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import styles from './ProductPage.module.css'
 import filledStar from '../assets/fullStar.png'
 import notFilledStar from '../assets/notFilledStar.png'
+import { useState } from 'react'
+import picture from '../assets/winter.png'
+import pic2 from '../assets/example.png'
+
+
 const ProductPage = () => {
-    const params = useParams();
+  const params = useParams();
+
+  const [quantity, setQuantity] = useState(1);
+
+  const [src,setSrc] = useState("/src/assets/winter.png");
+  const [selected,setSelected] =useState("");
+
+  function increment() {
+    setQuantity(prev => prev + 1);
+  }
+
+  function decrement() {
+    if (quantity > 1) {
+      setQuantity(prev => prev - 1);
+    }
+  }
+
   return (
     <div className={styles.Container}>
       <div className={styles.left}>
         <div className={styles.mainImage}>
-          <img src="" alt="" />
+          <img src={src} alt="Main picture" />
         </div>
         <div className={styles.subImages}>
-            <ul>
-                <li><img src="" alt="picture1" /></li>
-                <li><img src="" alt="picture2" /></li>
-                <li><img src="" alt="picture3" /></li>
-            </ul>
+          <ul>
+            <li onMouseOver={(e)=> setSelected("one")} className={selected=="one"?styles.border:""}><img src="/src/assets/winter.png" alt="picture1" onMouseOver={(e)=> setSrc(e.target.src)} /></li>
+            <li onMouseOver={(e)=> setSelected("two")} className={selected=="two"?styles.border:""}><img src="/src/assets/example.png"alt="picture2" onMouseOver={(e)=> setSrc(e.target.src)} /></li>
+            <li onMouseOver={(e)=> setSelected("three")} className={selected=="three"?styles.border:""}><img src="/src/assets/winter.png" alt="picture3" onMouseOver={(e)=> setSrc(e.target.src)} /></li>
+          </ul>
         </div>
       </div>
       <div className={styles.right}>
@@ -25,15 +46,34 @@ const ProductPage = () => {
         </div>
         <div className={styles.ratings}>
           <div className={styles.stars}>
-          <img src={filledStar} alt="Full-star" className={styles.star} />
-          <img src={filledStar} alt="Full-star" className={styles.star} />
-          <img src={filledStar} alt="Full-star" className={styles.star} />
-          <img src={filledStar} alt="Full-star" className={styles.star} />
-          <img src={notFilledStar} alt="Notfilled-star" className={styles.star} />
+            <img src={filledStar} alt="Full-star" className={styles.star} />
+            <img src={filledStar} alt="Full-star" className={styles.star} />
+            <img src={filledStar} alt="Full-star" className={styles.star} />
+            <img src={filledStar} alt="Full-star" className={styles.star} />
+            <img src={notFilledStar} alt="Notfilled-star" className={styles.star} />
           </div>
           <div className={styles.ratingText}>
             <p>Ratings 100</p>
           </div>
+        </div>
+        <div className={styles.pricing}>
+          <p className={styles.finalPrice}>Rs.1800</p>
+          <div className={styles.priceNDiscount}>
+            <del className={styles.initialPrice}>Rs.2000</del>
+            <span className={styles.discount}>-10%</span>
+          </div>
+        </div>
+        <div className={styles.quantitySection}>
+          <p className={styles.text}>Quantity</p>
+          <div className={styles.updater}>
+            <button className={`${styles.btn} ${quantity===1?styles.transparent:""}`} onClick={() => decrement()}>-</button>
+            <p className={styles.quantity}>{quantity}</p>
+            <button className={styles.btn} onClick={() => increment()}>+</button>
+          </div>
+        </div>
+        <div className={styles.purchase}>
+          <button className={`${styles.buyBtn} ${styles.cBtn}`}>Buy Now</button>
+          <button className={`${styles.cartBtn} ${styles.cBtn}`}>Add to Cart</button>
         </div>
       </div>
     </div>
