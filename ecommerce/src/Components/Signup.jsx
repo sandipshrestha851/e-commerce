d9af4b4a5a5969ca839cd91f3a5ebc1638eb5643
sandipshres <sandipshrestha4455@gmail.com>
2026-03-06 17:03:27 +0545

    validity function added to the form

--- ecommerce/src/Components/Signup.jsx
+++ ecommerce/src/Components/Signup.jsx
@@ -1,16 +1,50 @@
 import React from 'react'
+import {useState} from 'react'
 import styles from "./Signup.module.css"
 
 const Signup = () => {
+
+    const [email,setEmail] = useState("");
+    const [pass,setPass] = useState("");
+    const [confirm,setConfirm] = useState("");
+    const [checkPass,setCheckPass] = useState(true);
+
+
+    function updateEmail(e) {
+        let value = e.target.value;
+        setEmail(value);
+        localStorage.setItem("email",value);
+    }
+
+    function updatePass(e) {
+        setPass(e.target.value);
+        localStorage.setItem("password",pass);
+    }
+
+    function updateConfirm(e) {
+        setConfirm(e.target.value);
+    }
+
+    function Validity(e){
+        
+        if(pass!=confirm){
+            setCheckPass(false);
+            e.preventDefault();
+            return false;
+        }                             
+            setCheckPass(true);
+    }
+
     return (
         <div className={styles.Root}>
             <div className={styles.Container}>
                 <h1 className={styles.Heading}>Sign Up</h1>
-                <form className={styles.formInputs} action="">
-                    <input type="email" id="email" placeholder="Email" /><br />
-                    <input type="password" id="password" placeholder="Password" /><br />
-                    <input type="password" id="confirm" placeholder="Confirm Password" />
-                <button className={styles.signupBtn}>Sign Up</button>
+                <form onSubmit={Validity} className={styles.formInputs} >
+                    <input type="email" id="email" placeholder="Email" onChange={updateEmail} /><br />
+                    <input type="password" id="password" placeholder="Password" onChange={updatePass} /><br />
+                    <input type="password" id="confirm" placeholder="Confirm Password" onChange={updateConfirm}  />
+                    <p className={checkPass?styles.invisible:styles.visible}>Password doesnot match!</p>
+                <button type="submit" className={styles.signupBtn}>Sign Up</button>
                 </form>
             </div>
         </div>
